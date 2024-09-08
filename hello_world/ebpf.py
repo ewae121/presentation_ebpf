ea474ab34789803205b0d05571255f4c5aa6e3db
Laurent B <laurent041@gmail.com>
2024-09-08 17:44:01 +0200

    feat: add example

---
+++ hello_world/ebpf.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+from bcc import BPF
+from time import sleep
+
+program = """
+int hello_world(void *ctx) {
+    bpf_trace_printk("Hello, World!\\n");
+    return 0;
+}
+"""
+
+program_uid = """
+int hello_world(void *ctx) {
+    u64 uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+
+    bpf_trace_printk("id: %d\\n", uid);
+    return 0;
+}
+"""
+
+# b = BPF(text=program_uid)
+# clone = b.get_syscall_fnname("clone")
+# b.attach_kprobe(event=clone, fn_name="hello_world")
+# b.trace_print()
+
+program_map = """
+BPF_HASH(clones);
+
+int hello_world(void *ctx) {
+    u64 uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+    u64 counter = 0;
+    u64 *p;
+
+    p = clones.lookup(&uid);
+    if (p != 0) {
+        counter = *p;
+    }
+
+    counter++;
+    clones.update(&uid, &counter);
+
+    return 0;
+}
+"""
+
+b = BPF(text=program_map)
+clone = b.get_syscall_fnname("clone")
+b.attach_kprobe(event=clone, fn_name="hello_world")
+
+while True:
+    sleep(1)
+    s = ""
+    if b["clones"]:
+        for k, v in b["clones"].items():
+            s += f"ID: {k.value}, Clones: {v.value}\t"
+        print(s)
+    else:
+        print("No clones found")
+
